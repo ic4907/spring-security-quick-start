5d339ed7ef0dc7fad74708b4716442c94f4b1edf
Yonggang Wang <ygwang@thoguthworks.com>
2018-09-26 16:20:06 +0800

    make more antMatchers

--- src/main/java/com/orgdown/spring/config/SecurityConfig.java
+++ src/main/java/com/orgdown/spring/config/SecurityConfig.java
@@ -3,14 +3,16 @@ package com.orgdown.spring.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
@@ -21,7 +23,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/demos").permitAll();
+                .antMatchers("/demos").permitAll()
+                .antMatchers("/ip-based").hasIpAddress("192.168.0/24")
+                .antMatchers("/users/me").hasAnyRole("USER")
+                .antMatchers("/user-detail").hasAuthority("READ_USER_DETAIL")
+                .anyRequest().authenticated();
     }
 
     @Override
